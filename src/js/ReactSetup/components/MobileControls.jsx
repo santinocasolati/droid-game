ca7192cc0d89449aeac37d02be7c1c3184b57d5f
santinocasolati <santicasolati@gmail.com>
2022-09-28 04:39:54 -0300

    Mobile Controls Fixed

--- src/js/ReactSetup/components/MobileControls.jsx
+++ src/js/ReactSetup/components/MobileControls.jsx
@@ -3,26 +3,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faArrowDown, faArrowLeft, faArrowRight, faAnglesUp } from "@fortawesome/free-solid-svg-icons";
 
 export function MobileControls() {
+    const keysPressed = window.webgl.controls.keysPressed;
+
     const touchStart = (key) => {
         switch (key) {
-            case 'w':
-                window.webgl.playerControls.keysPressed.w = true;
+            case 'forward':
+                keysPressed.forward = true;
                 break;
 
-            case 's':
-                window.webgl.playerControls.keysPressed.s = true;
+            case 'backward':
+                keysPressed.backward = true;
                 break;
 
-            case 'a':
-                window.webgl.playerControls.keysPressed.a = true;
+            case 'left':
+                keysPressed.left = true;
                 break;
 
-            case 'd':
-                window.webgl.playerControls.keysPressed.d = true;
+            case 'right':
+                keysPressed.right = true;
                 break;
 
-            case 'space':
-                window.webgl.playerControls.keysPressed.space = true;
+            case 'jump':
+                keysPressed.jump = true;
                 break;
 
             default:
@@ -32,24 +34,24 @@ export function MobileControls() {
 
     const touchEnd = (key) => {
         switch (key) {
-            case 'w':
-                window.webgl.playerControls.keysPressed.w = false;
+            case 'forward':
+                keysPressed.forward = false;
                 break;
 
-            case 's':
-                window.webgl.playerControls.keysPressed.s = false;
+            case 'backward':
+                keysPressed.backward = false;
                 break;
 
-            case 'a':
-                window.webgl.playerControls.keysPressed.a = false;
+            case 'left':
+                keysPressed.left = false;
                 break;
 
-            case 'd':
-                window.webgl.playerControls.keysPressed.d = false;
+            case 'right':
+                keysPressed.right = false;
                 break;
 
-            case 'space':
-                window.webgl.playerControls.keysPressed.space = false;
+            case 'jump':
+                keysPressed.jump = false;
                 break;
 
             default:
@@ -61,25 +63,25 @@ export function MobileControls() {
         <div className={document.querySelector('html').dataset.device === 'desktop' ? 'mobile-controls off' : 'mobile-controls'}>
 
             <div className="movement">
-                <div className='up' onTouchStart={() => touchStart('w')} onTouchEnd={() => touchEnd('w')}>
+                <div className='up' onTouchStart={() => touchStart('forward')} onTouchEnd={() => touchEnd('forward')}>
                     <FontAwesomeIcon icon={faArrowUp} />
                 </div>
 
-                <div className='down' onTouchStart={() => touchStart('s')} onTouchEnd={() => touchEnd('s')}>
+                <div className='down' onTouchStart={() => touchStart('backward')} onTouchEnd={() => touchEnd('backward')}>
                     <FontAwesomeIcon icon={faArrowDown} />
                 </div>
             </div>
 
             <div className="direction">
-                <div className="left" onTouchStart={() => touchStart('a')} onTouchEnd={() => touchEnd('a')}>
+                <div className="left" onTouchStart={() => touchStart('left')} onTouchEnd={() => touchEnd('left')}>
                     <FontAwesomeIcon icon={faArrowLeft} />
                 </div>
 
-                <div className="right" onTouchStart={() => touchStart('d')} onTouchEnd={() => touchEnd('d')}>
+                <div className="right" onTouchStart={() => touchStart('right')} onTouchEnd={() => touchEnd('right')}>
                     <FontAwesomeIcon icon={faArrowRight} />
                 </div>
 
-                <div className="jump" onTouchStart={() => touchStart('space')} onTouchEnd={() => touchEnd('space')}>
+                <div className="jump" onTouchStart={() => touchStart('jump')} onTouchEnd={() => touchEnd('jump')}>
                     <FontAwesomeIcon icon={faAnglesUp} />
                 </div>
             </div>
